a4946ba0a5d9c61de82a36602d3e70829f1a9ef9
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-12-05 16:32:57 +0200

    Refactor Auth form

--- src/components/forms/AuthFormContainer/index.js
+++ src/components/forms/AuthFormContainer/index.js
@@ -4,26 +4,32 @@ import React from 'react'
 import styles from './AuthFormContainer.module.scss'
 
 export default function AuthFormContainer(props) {
-  const { header, submitText, children, initialValues, onSubmit, validationSchema } = props;
+  const { header, submitText, children } = props;
+  const { initialValues, onSubmit, validationSchema } = props.initForm;
 
   const formContainerClasses = classNames(
     styles.formContainer,
     'col-11 col-md-8 col-lg-7 col-xl-5',
-  )
+  );
+
+  console.log(props);
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-      <div className='container'>
-        <div className='row'>
-          <div className={formContainerClasses}>
-            <h2 className={styles.header}>{header}</h2>
+    <div className='container'>
+      <div className='row'>
+        <div className={formContainerClasses}>
+          {header && <div>
+            {header.mainHeader && <h2 className={styles.header}>{header.mainHeader}</h2>}
+            {header.subHeader && <h3 className={styles.subHeader}>{header.subHeader}</h3>}
+          </div>}
+          <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
             <Form>
               {children}
               <input className={styles.submit} type="submit" value={submitText} />
             </Form>
-          </div>
+          </Formik>
         </div>
       </div>
-    </Formik>
+    </div>
   )
 }
